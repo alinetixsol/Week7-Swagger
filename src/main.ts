62b1b4cce550978f9d481d1c657aa26b6fa8e82a
alinetixsol <ali_muhammad_intern@netixsol.com>
2024-02-22 13:06:32 +0500

    final

--- src/main.ts
+++ src/main.ts
@@ -36,7 +36,6 @@ async function bootstrap() {
       'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
     ],
   });
-  SwaggerModule.setup('api', app, document);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
