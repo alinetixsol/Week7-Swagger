1e10aecacd5fb1c960ad507030f4cf6223d3f4b8
alinetixsol <ali_muhammad_intern@netixsol.com>
2024-02-23 11:43:25 +0500

    FinalCommit

--- src/book/dto/update-book.dto.ts
+++ src/book/dto/update-book.dto.ts
@@ -1,34 +1,33 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {Category} from '../schemas/book.schema';
-import { IsString, IsOptional, IsEnum, IsNumber  } from 'class-validator';
+import { Category } from '../schemas/book.schema';
+import { IsString, IsOptional, IsEnum, IsNumber } from 'class-validator';
 import { User } from '../../auth/schema/auth.schema';
 
-export class UpdateBookDto{
+export class UpdateBookDto {
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  readonly title: string;
 
-    @ApiProperty()
-    @IsOptional()
-    @IsString()
-    readonly title: string;
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  readonly description: string;
 
-    @ApiProperty()
-    @IsOptional()
-    @IsString()
-    readonly description: string;
+  @ApiProperty()
+  @IsOptional()
+  @IsString()
+  readonly author: string;
 
-    @ApiProperty()
-    @IsOptional()
-    @IsString()
-    readonly author: string;
+  @ApiProperty()
+  @IsOptional()
+  @IsNumber()
+  readonly price: number;
 
-    @ApiProperty()
-    @IsOptional()
-    @IsNumber()
-    readonly price: number;
+  @ApiProperty()
+  @IsOptional()
+  @IsEnum(Category, { message: 'Please enter correct category.' })
+  readonly category: Category;
 
-    @ApiProperty()
-    @IsOptional()
-    @IsEnum(Category, {message: "Please enter correct category."})
-    readonly category: Category;
-
-    user:User
+  user: User;
 }
